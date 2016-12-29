3a93b104bb1057dfe733274fd054e65a9ea5ba70
Lockeriano <patryk.skrzyniarz19@gmail.com>
2016-12-30 00:02:18 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+#game_container
+	height: 100vh
+  width: 100vw
+  border: 3px solid white
+  background-color: #94b8b8
+
+#word_board
+  position: absolute
+  width: 30vh
+  top: 0
+  background-color: #534343
